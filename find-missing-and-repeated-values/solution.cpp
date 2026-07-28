d6190983308580d86afcb7ab73022aea47f56414
Ronakp088 <ronakgolu007@gmail.com>
2026-07-28 18:30:52 +0530

    Add solution: Find Missing and Repeated Values

---
+++ find-missing-and-repeated-values/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+    int n = grid.size();
+    vector<int> count(n * n + 1, 0);
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            count[grid[i][j]]++;
+
+    int repeated = -1, missing = -1;
+
+    for (int i = 1; i <= n * n; i++) {
+        if (count[i] == 2) repeated = i;
+        if (count[i] == 0) missing = i;
+    }
+
+    return {repeated, missing};
+}
+};
