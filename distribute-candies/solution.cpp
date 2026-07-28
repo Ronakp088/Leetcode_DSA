e5b56cc88fd996c32e754bdb9e88ba0ef0ce209e
Ronakp088 <ronakgolu007@gmail.com>
2026-07-28 18:30:22 +0530

    Add solution: Distribute Candies

---
+++ distribute-candies/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> st;
+        for(int i=0;i<candyType.size();i++){
+            st.insert(candyType[i]);
+        }
+        if(st.size() > candyType.size()/2) return candyType.size()/2;
+        return st.size();
+    }
+};
