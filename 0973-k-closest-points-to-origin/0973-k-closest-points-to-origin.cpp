2c18b8d812ca19814ea823a28b8836bf83b95551
Ronakp088 <ronakgolu007@gmail.com>
2026-07-30 20:58:40 +0530

    Time: 43 ms (58.51%), Space: 66.8 MB (83.51%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int distance(vector<int>& t) { return t[0] * t[0] + t[1] * t[1]; }
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         int n = points.size();
-        priority_queue<pair<int, vector<int>>> pq;
+        priority_queue<pair<int,int>> pq;
         vector<vector<int>> ans;
 
         for (int i = 0; i < n; i++) {
 
-            pair<int, vector<int>> pi = {distance(points[i]), points[i]};
+            pair<int,int> pi = {distance(points[i]),i};
             pq.push(pi);
             if (pq.size() > k) {
                 pq.pop();
             }
         }
         while (!pq.empty()) {
-            ans.push_back(pq.top().second);
+            ans.push_back(points[pq.top().second]);
             pq.pop();
         }
 
