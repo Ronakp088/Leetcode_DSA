a59fb80c12455d45f433125d0e184f79e27155d3
Ronakp088 <ronakgolu007@gmail.com>
2026-07-31 09:41:13 +0530

    Time: 16 ms (33.38%), Space: 25.9 MB (55.63%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        vector<int> arr(26);
+        priority_queue<int> pq;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[word[i] - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > 0) {
+                pq.push(arr[i]);
+            }
+        }
+        int j = 0;
+        while (!pq.empty()) {
+            ans += pq.top() * (j / 8 + 1);
+            j++;
+            pq.pop();
+        }
+        return ans;
+    }
+};
