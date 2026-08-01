511bd819750c6d213f227bb435ef696805d6831e
Ronakp088 <ronakgolu007@gmail.com>
2026-08-01 18:33:04 +0530

    Time: 0 ms (100%), Space: 22.1 MB (83.12%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,17 +3,29 @@ public:
     int candy(vector<int>& nums) {
         int n =nums.size();
         if(n == 1) return 1;
-        int ans = 0;
-        vector<int> v1(n,1);
-        vector<int> v2(n,1);
-        for(int i =1 ;i<n;i++){
-            if(nums[i] > nums[i-1]) v1[i] = v1[i-1] +1;
-        }
-        for(int i= n-2;i>=0;i--){
-            if(nums[i]>nums[i+1]) v2[i] = v2 [i+1] +1;
-        }
-        for(int i=0;i<n;i++){
-            ans += max(v1[i],v2[i]);
+        int ans = 1;
+        int peak =0;
+        int down =0;
+        int i =1;
+        while(i<n){
+            if(nums[i] == nums[i-1]){
+                ans+=1;
+                i++;
+                continue;
+            }
+            peak =1;
+            while(i<n && nums[i] > nums[i-1]){
+                peak += 1;
+                ans += peak;
+                i++;
+            }
+            down =1;
+            while( i<n && nums[i] < nums[i-1]){
+                ans += down;
+                down += 1;
+                i++;
+            }
+            if(down > peak) ans += down-peak;
         }
         return ans;
     }
