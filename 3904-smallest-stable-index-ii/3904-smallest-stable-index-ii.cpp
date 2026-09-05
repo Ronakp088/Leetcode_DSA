0fec4d94c7b98d5ca74b57dab7818aa43897b77f
Ronakp088 <ronakgolu007@gmail.com>
2026-09-05 10:35:11 +0530

    Time: 7 ms (77.75%), Space: 202.5 MB (85.02%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -5,20 +5,21 @@ public:
         if( n == 1){
              return 0;
         }
-        vector<pair<int,int>> v(n);
-        v[0].first = nums[0];
-        v[n-1].second = nums[n-1];
-        for(int i=1;i<=n-2;i++){
-        v[i].first = max(nums[i],v[i-1].first);
-        v[n-1-i].second = min(nums[n-1-i],v[n-i].second);
+        vector<int> v(n);
+        v[0] = nums[0];
+        for(int i =1;i<n;i++){
+            v[i] = max(nums[i],v[i-1]);
         }
-        v[n-1].first = max(nums[n-1],v[n-2].first);
-        v[0].second = min(nums[0],v[1].second);
-
-        for(int i =0;i<n;i++){
-            if((v[i].first-v[i].second) <= k) return i;
+        int mini = nums[n-1];
+        v[n-1] = v[n-1]-mini;
+        int ans = -1;
+        if(v[n-1] <= k) ans = n-1;
+        for(int i =n-2;i>=0;i--){
+            mini = min(nums[i],mini);
+            v[i] = v[i] - mini;
+            if(v[i] <= k) ans =i;
         }
-
-        return -1;
+        
+        return ans;
     }
 };
