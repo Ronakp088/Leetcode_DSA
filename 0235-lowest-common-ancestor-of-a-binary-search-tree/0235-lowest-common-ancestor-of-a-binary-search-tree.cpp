7f0ede446debaa56a22c31c76e6d6e212cd9ab7b
Ronakp088 <ronakgolu007@gmail.com>
2026-09-07 21:36:03 +0530

    Time: 23 ms (57.06%), Space: 23.4 MB (91.29%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,45 +11,13 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> vp;
-        vector<TreeNode*> vq;
-        TreeNode* nodep = root;
-        TreeNode* nodeq = root;
-        if(nodep->val == p->val) vp.push_back(nodep);
-        else
-        {
-            while(nodep->val != p->val){
-                vp.push_back(nodep);
-                if(p->val > nodep->val){
-                    nodep = nodep->right;
-                }
-                else{
-                    nodep = nodep->left;
-                }
-            }
-            vp.push_back(nodep);
-         }
-
-        if(nodeq->val == q->val) vq.push_back(nodeq);
-        else
-        {
-            while(nodeq->val != q->val){
-                vq.push_back(nodeq);
-                if(q->val > nodeq->val){
-                    nodeq = nodeq->right;
-                }
-                else{
-                    nodeq = nodeq->left;
-                }
-            }
-            vq.push_back(nodeq);
-         }
-
-        int i =0;
-        while(i < vp.size() && i < vq.size() && vp[i]->val == vq[i]->val){
-            i++;
+        if(root == NULL) return root;
+        while(root){
+            if(root == p || root == q) return root ;
+            if(root->val > p->val && root->val > q->val) root = root->left ;
+            else if(root->val < p->val && root->val < q->val) root = root->right ;
+            else return root ;
         }
-        i--;
-        return vp[i];
+        return root ;
     }
 };
