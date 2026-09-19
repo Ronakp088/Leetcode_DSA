b0692c4563104221957962d74ef68a1c8b61a8c0
Ronakp088 <ronakgolu007@gmail.com>
2026-09-19 19:09:57 +0530

    Time: 0 ms (100%), Space: 18.9 MB (5.15%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,18 +4,16 @@ public:
         int n = image.size();
         int m = image[0].size();
         if( i < 0 || j < 0 || i == n || j == m ) return;
-        if(image[i][j] == prev && !visited[i][j]){
+        if(visited[i][j] == 1) return;
         visited[i][j] = 1;
+        if(image[i][j] == prev){
         image[i][j] = color;
         dfs(image,visited,i-1,j,color, prev);
         dfs(image,visited,i+1,j,color, prev);
         dfs(image,visited,i,j-1,color, prev);
         dfs(image,visited,i,j+1,color, prev);
         }
-        else {
-            visited[i][j] = 1;
-            return;
-        }
+       return;
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size(); // row
